9e7107be3ca0ef5157da760b6d9b77e9b9030e9b
Maksym <maxkord2003@gmail.com>
2024-04-12 14:22:58 +0300

    Commit

--- main.py
+++ main.py
@@ -1,16 +1,51 @@
-# This is a sample Python script.
+import datetime
+import json
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import aiohttp
+import asyncio
+import sys
 
+class CurrencyAPI:
+    def __init__(self):
+        self.base_url = "https://api.privatbank.ua/p24api/exchange_rates"
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    async def fetch_data(self, date):
+        try:
+            async with aiohttp.ClientSession() as session:
+                async with session.get(f"{self.base_url}?&date={date.strftime('%d.%m.%Y')}") as response:
+                    return await response.json()
+        except aiohttp.ClientError as e:
+            print(f"Network error: {e}")
 
+    async def get_exchange_rates(self, dates):
+        tasks = [self.fetch_data(date) for date in dates]
+        return await asyncio.gather(*tasks)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+async def main(days):
+    today = datetime.date.today()
+    dates = [today - datetime.timedelta(days=i) for i in range(days)]
+    api = CurrencyAPI()
+    exchange_rates = await api.get_exchange_rates(dates)
+    return exchange_rates
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python main.py <number_of_days>")
+        sys.exit(1)
+    try:
+        days = int(sys.argv[1])
+        if days <= 0 or days > 10:
+            raise ValueError("Number of days should be between 1 and 10")
+    except ValueError as e:
+        print("Invalid input:", e)
+        sys.exit(1)
+    result = asyncio.run(main(days))
+
+    currency_data = []
+    for item in result:
+        for exchRate in item['exchangeRate']:
+            if exchRate['currency'] == 'EUR' or exchRate['currency'] == 'USD':
+                currency_data.append(exchRate)
+    formatted_result = json.dumps(currency_data, indent=2)
+    print(formatted_result)
+
